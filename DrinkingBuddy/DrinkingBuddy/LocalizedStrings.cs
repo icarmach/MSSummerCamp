26572d78922a1542ef654495ff6400c4abf02dd7
Ignacio Carmach <icarmach@gmail.com>
2014-01-23 13:19:41 -0300

    proyecto

---
+++ DrinkingBuddy/DrinkingBuddy/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using DrinkingBuddy.Resources;
+
+namespace DrinkingBuddy
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
