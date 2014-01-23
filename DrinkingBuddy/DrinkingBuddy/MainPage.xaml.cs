b229a0a7c4fe68fb234403f43ef9b8ec1a7b7347
Ignacio Carmach <icarmach@gmail.com>
2014-01-23 13:58:54 -0300

    navegacion

--- DrinkingBuddy/DrinkingBuddy/MainPage.xaml.cs
+++ DrinkingBuddy/DrinkingBuddy/MainPage.xaml.cs
@@ -22,6 +22,11 @@ namespace DrinkingBuddy
             //BuildLocalizedApplicationBar();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1s.xaml", UriKind.Relative));
+        }
+
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
         //{
