201383f6e8afb9265d891c5c4ed0a509be78f395
LestatAlmasy <lestat__x@hotmail.com>
2014-01-23 17:45:22 -0300

    Tips
    Tiiiips

--- DrinkingBuddy/DrinkingBuddy/Menu.xaml.cs
+++ DrinkingBuddy/DrinkingBuddy/Menu.xaml.cs
@@ -12,9 +12,44 @@ namespace DrinkingBuddy
 {
     public partial class Menu : PhoneApplicationPage
     {
+        public static int[] arreglo = new int [] {0,0,0,0,0};
+
         public Menu()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          NavigationService.Navigate(new Uri("/Tips.xaml", UriKind.Relative));
+        }
+
+        private void Cabeza_check_Checked(object sender, RoutedEventArgs e)
+        {
+            arreglo[0] = 1;
+        }
+
+        private void Estomago_check_Checked(object sender, RoutedEventArgs e)
+        {
+            arreglo[1] = 1;
+        }
+
+        private void Mareo_check_Checked(object sender, RoutedEventArgs e)
+        {
+            arreglo[2] = 1;
+        }
+
+        private void Vomito_check_Checked(object sender, RoutedEventArgs e)
+        {
+            arreglo[3] = 1;
+        }
+
+        private void Zombie_check_Checked(object sender, RoutedEventArgs e)
+        {
+            arreglo[4] = 1;
+        }
+
+        
+        
     }
 }
---
+++ DrinkingBuddy/DrinkingBuddy/Tips.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+
+namespace DrinkingBuddy
+{
+
+    public partial class Tips : PhoneApplicationPage
+    {
+        
+        string TipCabeza = "Toma un vaso de agua ";
+        string TipEstomago = "Recuestate";
+        string TipMareo = "Toma aguita de hiervas ";
+        string TipVomito = "Sigue ";
+        string TipZombie = "Adiós amigo ...";
+        public Tips()
+        {
+            
+            InitializeComponent();
+            int [] arreglo2 = Menu.arreglo;
+            
+        }
+    }
+}
