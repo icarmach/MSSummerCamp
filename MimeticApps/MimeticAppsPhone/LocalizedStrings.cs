d28802c1eed2795edebc06b5165d6ba24f7f7daa
Ignacio Carmach <icarmach@gmail.com>
2014-01-30 11:50:11 -0300

    nueva app

---
+++ MimeticApps/MimeticAppsPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MimeticAppsPhone.Resources;
+
+namespace MimeticAppsPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
